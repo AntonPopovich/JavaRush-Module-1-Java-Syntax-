4aa9309ee2d83eefae4ab7dacc70f6da9858f33d
AntonPopovich <106447068+AntonPopovich@users.noreply.github.com>
2022-06-05 12:24:50 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,3 @@
-
-/*
-Online Java - IDE, Code Editor, Compiler
-
-Online Java is a quick and easy tool that helps you to build, compile, test your programs online.
-*/
-
 import java.io.IOException;
 import java.util.Scanner;
 
